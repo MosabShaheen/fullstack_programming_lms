af80e22c6e7567fe2116e61918399a65d0906542
MosabShaheen <mosabshaheen62@gmail.com>
2025-01-07 03:14:48 +0500

    created simple homepage for the laptop and big screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
+    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-40 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center ">
+        {/* <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        /> */}
+        <h1 className="text-center leading-[1.3] uppercase font-bold text-7xl">Welcome to Full-Stack Programming Course</h1>
+        {/* <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
@@ -21,7 +22,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-        </ol>
+        </ol>*/}
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -37,7 +38,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Register now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
@@ -45,14 +46,14 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Login To LMS
           </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://wa.me/923360200088"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -63,12 +64,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          03360200088
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
+          href="mailto:info@taibainstitute.pk"
           rel="noopener noreferrer"
         >
           <Image
@@ -78,11 +78,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          info@taibainstitute.pk
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://taibainstitute.pk/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -93,7 +93,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to taibainstitute.pk →
         </a>
       </footer>
     </div>
